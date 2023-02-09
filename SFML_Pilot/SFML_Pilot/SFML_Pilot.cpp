6d297678cbbdb479123f12dd21153fd8ef152b6e
rahul-begames <60950973+RahulChandra99@users.noreply.github.com>
2023-02-09 22:10:36 +0530

    window creation

--- SFML_Pilot/SFML_Pilot/SFML_Pilot.cpp
+++ SFML_Pilot/SFML_Pilot/SFML_Pilot.cpp
@@ -1,23 +1,32 @@
 #include <SFML/Graphics.hpp>
+#include<iostream>
+
+#define WIDTH 1024
+#define HEIGHT 1024
+
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT),"TicTacToe");
 
     while (window.isOpen())
     {
         sf::Event event;
+
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            switch (event.type)
+            {
+            case sf::Event::Closed:
                 window.close();
-        }
 
+                break;
+            }
+        }
         window.clear();
-        window.draw(shape);
+
         window.display();
+
     }
 
     return 0;
